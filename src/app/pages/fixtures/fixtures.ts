24d48c9e2341c5be090a6fa56432892fe235d5b6
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-09-02 12:35:07 +0100

    feat: implement fixture card component with styling and tests; update fixtures page to use fixture card component

--- src/app/pages/fixtures/fixtures.ts
+++ src/app/pages/fixtures/fixtures.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { FixtureService } from '../../services/fixture';
+import { FixtureCard } from '../../components/fixture-card/fixture-card';
 import { AsyncPipe, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-fixtures',
-  imports: [AsyncPipe, DatePipe],
+  imports: [AsyncPipe, DatePipe, FixtureCard],
   templateUrl: './fixtures.html',
   styleUrl: './fixtures.css',
 })
