44a526e7438a5de206e5621741ede852878c3b52
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-09-04 15:22:21 +0100

    feat: restructure league table component and template for improved readability

--- src/app/pages/league-table/league-table.ts
+++ src/app/pages/league-table/league-table.ts
@@ -1,28 +1,31 @@
 import { Component, inject } from '@angular/core';
 
 import { League } from '../../services/league';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-league-table',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './league-table.html',
   styleUrl: './league-table.css',
 })
 export class LeagueTable {
   private leagueService = inject(League);
 
-  constructor() {
-    this.loadLeagueTable();
-  }
+  // constructor() {
+  //   this.loadLeagueTable();
+  // }
 
-  loadLeagueTable(): void {
-    this.leagueService.getLeagueTable().subscribe({
-      next: (response) => {
-        console.log('LEAGUE API SUCCESS:', response);
-      },
-      error: (error) => {
-        console.error('LEAGUE API ERROR:', error);
-      },
-    });
-  }
+  // loadLeagueTable(): void {
+  //   this.leagueService.getLeagueTable().subscribe({
+  //     next: (response) => {
+  //       console.log('LEAGUE API SUCCESS:', response);
+  //     },
+  //     error: (error) => {
+  //       console.error('LEAGUE API ERROR:', error);
+  //     },
+  //   });
+  // }
+
+  leagueTable$ = this.leagueService.getLeagueTable();
 }
