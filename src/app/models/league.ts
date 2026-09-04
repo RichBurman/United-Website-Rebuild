2161522349b431d63930de632efb048517ae3758
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-09-04 14:56:54 +0100

    feat: add LeagueTeam model and update League service to fetch and map league data

---
+++ src/app/models/league.ts
@@ -0,0 +1,12 @@
+export interface LeagueTeam {
+  position: number;
+  teamName: string;
+  playedGames: number;
+  won: number;
+  draw: number;
+  lost: number;
+  goalsFor: number;
+  goalsAgainst: number;
+  goalDifference: number;
+  points: number;
+}
--- src/app/services/league.ts
+++ src/app/services/league.ts
@@ -1,9 +1,10 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 import { environment } from '../../environments/environment';
 import { LeagueApiResponse } from '../models/league-api';
+import { LeagueTeam } from '../models/league';
 
 @Injectable({
   providedIn: 'root',
@@ -11,14 +12,28 @@ import { LeagueApiResponse } from '../models/league-api';
 export class League {
   private http = inject(HttpClient);
 
-  getLeagueTable(): Observable<LeagueApiResponse> {
+  getLeagueTable(): Observable<LeagueTeam[]> {
     const headers = new HttpHeaders({
       'X-Auth-Token': environment.apiToken,
     });
 
-    return this.http.get<LeagueApiResponse>(
-      '/v4/competitions/PL/standings',
-      { headers },
-    );
+    return this.http
+      .get<LeagueApiResponse>('/api/v4/competitions/PL/standings', { headers })
+      .pipe(
+        map((response) =>
+          response.standings[0].table.map((team) => ({
+            position: team.position,
+            teamName: team.team.shortName,
+            playedGames: team.playedGames,
+            won: team.won,
+            draw: team.draw,
+            lost: team.lost,
+            goalsFor: team.goalsFor,
+            goalsAgainst: team.goalsAgainst,
+            goalDifference: team.goalDifference,
+            points: team.points,
+          })),
+        ),
+      );
   }
 }
