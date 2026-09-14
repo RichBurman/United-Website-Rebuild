1f1a86da930022672e43b3f23377ab5ef7cf21a3
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-09-14 11:04:56 +0100

    feat: implement blog post component with dynamic content and styling

--- src/app/pages/blog-post/blog-post.ts
+++ src/app/pages/blog-post/blog-post.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { DatePipe } from '@angular/common';
+
+import { BlogService } from '../../services/blog/blog.service.ts';
+
 
 @Component({
   selector: 'app-blog-post',
-  imports: [],
+  imports: [DatePipe, RouterLink],
   templateUrl: './blog-post.html',
   styleUrl: './blog-post.css',
 })
 export class BlogPost {
+  private route = inject(ActivatedRoute);
+  private blogService = inject(BlogService);
 
+  slug = this.route.snapshot.paramMap.get('slug');
+
+  post = this.slug
+    ? this.blogService.getPostBySlug(this.slug)
+    : undefined;
 }
