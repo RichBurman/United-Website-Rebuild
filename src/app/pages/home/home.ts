35843364b475c1701bbc444d546c253b9c9c620c
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-09-15 11:37:26 +0100

    feat: restructure home page layout and implement fixture loading functionality

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 
+import { Fixture } from '../../models/fixture';
+import { FixtureService } from '../../services/fixture/fixture';
 @Component({
   selector: 'app-home',
   imports: [],
@@ -7,5 +9,36 @@ import { Component } from '@angular/core';
   styleUrl: './home.css',
 })
 export class Home {
+  private fixtureService = inject(FixtureService);
 
+  fixtures = signal<Fixture[]>([]);
+
+  lastResult = computed(() => {
+    const finishedMatches = this.fixtures().filter(
+      (fixture) => fixture.status === 'FINISHED',
+    );
+
+    return finishedMatches.at(-1);
+  });
+
+  nextMatch = computed(() => {
+    return this.fixtures().find(
+      (fixture) => fixture.status === 'UPCOMING',
+    );
+  });
+
+  constructor() {
+    this.loadFixtures();
+  }
+
+  loadFixtures() {
+    this.fixtureService.getFixtures().subscribe({
+      next: (fixtures) => {
+        this.fixtures.set(fixtures);
+      },
+      error: (error) => {
+        console.error('Unable to load home fixtures:', error);
+      },
+    });
+  }
 }
